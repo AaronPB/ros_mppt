83ecade6296a35257c07fc556ea91009d0830d6c
AaronPB <arp629@gmail.com>
2019-05-16 16:42:30 +0200

    sender script

--- scripts/vemppt_ros.py
+++ scripts/vemppt_ros.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+"""
+    This file is part of ros_mppt.
+    ros_mppt is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    vemppt_reader is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with ros_mppt.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import rospy
 import time
 import serial
